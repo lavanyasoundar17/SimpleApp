62686a04bcaddc87ea400c0946be7db69060d2f9
lavanyasoundar17 <lavanyaprdp17@gmail.com>
2024-12-09 20:46:49 +0000

    Connected H2 database and modified logics

--- src/main/java/com/Lav/SimpleApp/model/Product.java
+++ src/main/java/com/Lav/SimpleApp/model/Product.java
@@ -1,17 +1,19 @@
 package com.Lav.SimpleApp.model;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Component;
+import jakarta.persistence.Id;
 
 @Data
-@AllArgsConstructor
 
+@Component
+@Entity
 public class Product {
+  @Id
     private int prodId;
     private String prodName;
     private int price;
-
-
 
 }
--- src/main/java/com/Lav/SimpleApp/service/ProductService.java
+++ src/main/java/com/Lav/SimpleApp/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.Lav.SimpleApp.service;
 
+import com.Lav.SimpleApp.Repository.ProductRepo;
 import com.Lav.SimpleApp.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,37 +10,45 @@ import java.util.Arrays;
 import java.util.List;
 @Service
 public class ProductService {
+    @Autowired
+    ProductRepo repo;
 
-    List<Product> products = new ArrayList<>(Arrays.asList(new Product(101,"Iphone",50000), new Product(102,"Camera",70000), new Product(103,"mic",5000)));
+    //List<Product> products = new ArrayList<>(Arrays.asList(new Product(101,"Iphone",50000), new Product(102,"Camera",70000), new Product(103,"mic",5000)));
     public List<Product> getProducts() {
-        return products;
+        return repo.findAll();
     }
 
     public Product getProductById(int id) {
-        return products.stream().filter(p->p.getProdId()==id).findFirst().get();
+        //return products.stream().filter(p->p.getProdId()==id).findFirst().get();
+        Product product = repo.findById(id).orElse(new Product());
+        return product;
     }
 
     public void addProduct(Product prod) {
-        products.add(prod);
+        //products.add(prod);
+        repo.save(prod);
     }
 
     public void updateProduct(Product prod) {
-        int index = 0;
-        for (int i = index; i < products.size(); i++) {
-            if(products.get(i).getProdId()==prod.getProdId()) {
-                index = i;
-                products.set(index, prod);
-            }
-        }
+//        int index = 0;
+//        for (int i = index; i < products.size(); i++) {
+//            if(products.get(i).getProdId()==prod.getProdId()) {
+//                index = i;
+//                products.set(index, prod);
+//            }
+//        }
+        repo.save(prod);
     }
 
     public void deleteProduct(int prodId) {
-        int index =0;
-        for (int i = index; i < products.size(); i++) {
-            if(products.get(i).getProdId()==prodId) {
-                index = i;
-                products.remove(index);
-            }
-        }
+//        int index =0;
+//        for (int i = index; i < products.size(); i++) {
+//            if(products.get(i).getProdId()==prodId) {
+//                index = i;
+//                products.remove(index);
+//            }
+//        }
+        repo.deleteById(prodId);
+   }
+
     }
-}
